04a8b71ce0c176ad2bdd093efdf82556ac194069
luis10barbo <luis10barbo2020@gmail.com>
2022-09-01 12:49:51 -0300

    first commit

---
+++ src/bot.ts
@@ -0,0 +1,27 @@
+import { Client, GatewayIntentBits } from "discord.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+const token = process.env.BOT_TOKEN// "MTAxNDkwMDgxNjQwNDAyMTMwOA.GHmfHZ.SU15ESF08VV-2GeA6a9fjXotOAgvVNfA1FhpY4"
+const botId = "1014900816404021308"
+const client = new Client({intents:[GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`)
+})
+
+client.on("interactionCreate", interaction => {
+
+})
+
+client.on("messageCreate", (message) => {
+    if (message.channel.id !== "1014901140133007420") return
+
+    if (message.author.id !== botId) {
+        // Message is not from bot
+        message.reply("testezao")
+    }
+    console.log(message.content)
+})
+
+client.login(token)
